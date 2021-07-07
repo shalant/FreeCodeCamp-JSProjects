a3604f054eeb433e1f1e64f768057c258a63990d
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-07-07 11:59:12 -0500

    12-countdown, initial setup, working with dates and countdown math

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,36 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2022,3,25,11,30,0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60min
+  // 1d = 24hr
+}
+
+getRemainingTime();
