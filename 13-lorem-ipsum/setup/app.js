2f3c5fa0cc17c6f5eb8f5678b93d0cfc8ab1110e
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-07-08 10:03:18 -0500

    finished13-lorem

--- 13-lorem-ipsum/setup/app.js
+++ 13-lorem-ipsum/setup/app.js
@@ -18,5 +18,22 @@ const result = document.querySelector('.lorem-text');
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  console.log(e);
+  // strings are black, numbers are blue WTF!
+  const value = parseInt(amount.value);
+  const random = Math.floor(Math.random() * text.length);
+
+// empty
+// -1
+// > 9
+if(isNaN(value) || value <= 0 || value > 9) {
+  result.innerHTML = `<p class='result'>${text[random]}</p>`
+} else {
+  let tempText = text.slice(0,value);
+  tempText = tempText.map(function(item) {
+    return `<p class='result'>${item}</p>`;
+  })
+  .join('');
+result.innerHTML = tempText;
+}
+
 });
